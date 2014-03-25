bcc3491f483a70ea70b3a79ba410739083831a82
MY huang <huangmoyue@gmail.com>
2014-03-26 00:11:10 +0800

    updated
    updated

--- MaterialConsumptionPlan/Src/SQL/PL SQL/LearnPLSQL.sql
+++ MaterialConsumptionPlan/Src/SQL/PL SQL/LearnPLSQL.sql
@@ -22,7 +22,7 @@ BEGIN
   VALUES (name_in, date_in);
 END;
 
----Cursor Usage
+---------------------------------------Cursor Usage------------------------------------
 DECLARE
     v_name EMPLOYEE_NAME_BIRTHDAY.NAME%TYPE;
     CURSOR c_cursor IS SELECT NAME FROM EMPLOYEE_NAME_BIRTHDAY;
@@ -30,12 +30,47 @@ DECLARE
   BEGIN
     v_date := SYSDATE;
     OPEN c_cursor;
-    
     FETCH c_cursor INTO v_name;
-    
     SYS.DBMS_OUTPUT.PUT_LINE(v_name);
     UPDATE EMPLOYEE_NAME_BIRTHDAY SET BIRTHDAY = v_date WHERE NAME = v_name;
     CLOSE c_cursor;
 END;
 
+----------------------------------------String Usage-----------------------------------
 
+DECLARE
+  names          VARCHAR2(60) := 'Anna,Matt,Joe,Nathan,Andrew,Aaron,Jeff';
+  comma_location NUMBER       := 0;
+BEGIN
+  LOOP
+    comma_location := INSTR(names,',',comma_location+1);  ---comma_location + 1 means jump to next char
+    EXIT
+  WHEN comma_location = 0;
+    SYS.DBMS_OUTPUT.PUT_LINE(comma_location);
+  END LOOP;
+END;
+
+--Another case for string
+DECLARE
+  names          VARCHAR2(60) := 'Anna,Matt,Joe,Nathan,Andrew,Aaron,Jeff';
+  names_adjusted VARCHAR2(61);
+  comma_location NUMBER := 0;
+  prev_location  NUMBER := 0;
+BEGIN
+  --Stick a comma after the final name
+  names_adjusted := names || ',';
+  LOOP
+    comma_location := INSTR(names_adjusted,',',comma_location+1);
+    EXIT
+  WHEN comma_location = 0;
+    DBMS_OUTPUT.PUT_LINE( SUBSTR(names_adjusted,prev_location+1, comma_location-prev_location-1));
+    prev_location := comma_location;
+  END LOOP;
+END;
+
+--String replacement
+DECLARE
+  names VARCHAR2(60) := 'Anna,Matt,Joe,Nathan,Andrew,Aaron,Jeff';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE( REPLACE(names, ',', chr(10)) );
+END;
