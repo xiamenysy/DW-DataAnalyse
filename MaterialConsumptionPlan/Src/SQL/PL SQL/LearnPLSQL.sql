9430599bb68713e33c4282ab01da67d4ca5ffed8
MY huang <huangmoyue@gmail.com>
2014-04-03 20:19:31 +0800

    Add some plsql usage
    Add some plsql usage

--- MaterialConsumptionPlan/Src/SQL/PL SQL/LearnPLSQL.sql
+++ MaterialConsumptionPlan/Src/SQL/PL SQL/LearnPLSQL.sql
@@ -1,4 +1,4 @@
---The simplest PL&SQL script
+ --The simplest PL&SQL script
 BEGIN
   SYS.DBMS_OUTPUT.PUT_LINE('Hello PLSQL!');
 END;
@@ -22,6 +22,23 @@ BEGIN
   VALUES (name_in, date_in);
 END;
 
+--Display Function or Procedures
+-- PARA IN DATATYPE use in (dis in VARCHAR2)~!!!
+CREATE OR REPLACE PROCEDURE display_total_sales(dis in VARCHAR2)
+is
+begin
+  DBMS_OUTPUT.put_line(dis);
+end;
+
+-- How to Call proceduce
+-- So we always assign value in declare state ment
+DECLARE
+  start_year_in PLS_INTEGER := 10;  -- assign value
+  end_year_in  PLS_INTEGER := 20;
+BEGIN
+
+  display_multiple_years(start_year_in,end_year_in);
+END;
 
 ------------------------------------------loop----------------------------------------
 --File on web: loop_examples.sql
@@ -54,6 +71,34 @@ BEGIN
 END display_multiple_years;
 
 
+
+
+/* File on web: simple_integer_demo.sql */
+-- First create a compute intensive procedure using PLS_INTEGER
+-- Use For .... Loop....
+CREATE OR REPLACE PROCEDURE pls_test(
+    iterations IN PLS_INTEGER)
+AS
+  int1 PLS_INTEGER := 1;
+  int2 PLS_INTEGER := 2;
+  --USE TO CACL TIME USE...
+  begints TIMESTAMP;
+  endts   TIMESTAMP;
+BEGIN
+  begints := SYSTIMESTAMP;
+  FOR cnt IN 1 .. iterations
+  LOOP
+    int1 := int1 + int2 * cnt;
+    DBMS_OUTPUT.put_line(int1);
+  END LOOP;
+  endts := SYSTIMESTAMP;
+  DBMS_OUTPUT.put_line( iterations || ' iterations had run time of:' || TO_CHAR (endts - begints));
+END;
+
+
+BEGIN
+  pls_test(20000);
+END;
 
 
 
@@ -114,10 +159,10 @@ BEGIN
   SYS.DBMS_OUTPUT.PUT_LINE( RPAD(a,10,'.') || LPAD(b,10,'.') );
 END;
 
----Trim
+---String Trim
 DECLARE
   a VARCHAR2(40) := 'This sentence has too many periods......';
-  b VARCHAR2(40) := 'The number 1';
+  b VARCHAR2(40) := 'The nww22umber 1';
 BEGIN
   DBMS_OUTPUT.PUT_LINE( RTRIM(a,'.') );
   DBMS_OUTPUT.PUT_LINE( LTRIM(b, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz') );
@@ -199,6 +244,7 @@ END;
 
 --Count table row number
 select count(*) from INV_SAP_MATERIAL_CATALOG;
+select count(*) from INV_SAP_PP_OPTIMIZATION;
 
 select a.ROWID from INV_SAP_MATERIAL_CATALOG a;
 
@@ -282,4 +328,119 @@ WHERE a.MATERIALID = b.MATERIALID AND a.PLANTID = b.DIRECT_SHIP_PLANT;
 
 SELECT * FROM INV_SAP_VENDOR_PLANT_INFO where MATERIALID = 'PN-82272';
 
-select * from VIEW_VENDOR where MATERIALID = 'PN-82272';
+select * from INV_SAP_VENDOR_PLANT_INFO where MATERIALID = 'PN-82272' and plantid = '4000';
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------Numeric Datatypes------------------------------
+--NUMBER PLS_INTEGER BINARY_INTEGER  SIMPLE_INTEGER BINARY_FLOAT BINARY_DOUBLE
+--SIMPLE_FLOAT SIMPLE_DOUBLE
+
+DECLARE
+  tiny_nbr NUMBER := 1e-130;
+  test_nbr NUMBER;
+  -- 1111111111222222222233333333334
+  -- 1234567890123456789012345678901234567890
+  big_nbr NUMBER := 9.999999999999999999999999999999999999999e125;
+  -- 1111111111222222222233333333334444444
+  -- 1234567890123456789012345678901234567890123456
+  fmt_nbr VARCHAR2(50) := '9.99999999999999999999999999999999999999999EEEE';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('tiny_nbr =' || TO_CHAR(tiny_nbr, '9.9999EEEE'));
+  -- NUMBERs that are too small round down to zero
+  test_nbr := tiny_nbr / 1.0001;
+  DBMS_OUTPUT.PUT_LINE('tiny made smaller =' || TO_CHAR(test_nbr, fmt_nbr));
+  -- NUMBERs that are too large throw an error
+  DBMS_OUTPUT.PUT_LINE('big_nbr =' || TO_CHAR(big_nbr, fmt_nbr));
+  test_nbr := big_nbr * 1.0001; -- too big
+  DBMS_OUTPUT.PUT_LINE('big made bigger =' || TO_CHAR(test_nbr, fmt_nbr));
+END;
+
+
+DECLARE
+  test_number1 NUMBER(9,2) := 2222.222;
+  test_number2 NUMBER(9,-11) := 9999999999999999999;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('big made bigger =' || test_number2);
+END;
+
+DECLARE
+low_nbr NUMBER(38,127);
+high_nbr NUMBER(38,-84);
+BEGIN
+/* 127 is largest scale, so begin with 1 and move
+decimal point 127 places to the left. Easy. */
+low_nbr := 1E-127;
+DBMS_OUTPUT.PUT_LINE('low_nbr = ' || low_nbr);
+/* .84 is smallest scale value. Add 37 to normalize
+the scientific-notation, and we get E+121. */
+high_nbr := 9.9999999999999999999999999999999999999E+121;
+DBMS_OUTPUT.PUT_LINE('high_nbr = ' || high_nbr);
+END;
+  
+  
+DECLARE
+int1 PLS_INTEGER;
+int2 PLS_INTEGER;
+int3 PLS_INTEGER;
+nbr NUMBER;
+BEGIN
+int1 := 100;
+int2 := 49;
+int3 := int2/int1;
+nbr := int2/int1;
+DBMS_OUTPUT.PUT_LINE('integer 100/49 =' || TO_CHAR(int3));
+DBMS_OUTPUT.PUT_LINE('number 100/49 =' || TO_CHAR(nbr));
+int2 := 50;
+int3 := int2/int1;
+nbr := int2/int1;
+DBMS_OUTPUT.PUT_LINE('integer 100/50 =' || TO_CHAR(int3));
+DBMS_OUTPUT.PUT_LINE('number 100/50 =' || TO_CHAR(nbr));
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------FUNCTION---------------------------------
+--FUNCTION favorite_nickname (
+--name_in IN VARCHAR2) RETURN VARCHAR2
+--IS
+--BEGIN
+--...
+--END;
+
+
+
+
+
